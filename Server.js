b00da2c2c805c9ea0d759e6813c6d5f22da1b0d6
Oversight1 <87642379+Oversight1@users.noreply.github.com>
2025-07-24 09:15:55 +0100

    Updating Cors

--- Server.js
+++ Server.js
@@ -12,14 +12,19 @@ const PORT = 5000;
 const allowedOrigins = ["https://https://task-manager-frontend-gax5vfel1-oversight1s-projects.vercel.app"];
 //["https://task-manager-frontend-2yzezb2gh-oversight1s-projects.vercel.app"];
 // Middleware
+//app.use(cors({
+//  origin: function (origin, callback) {
+//    if (!origin || allowedOrigins.includes(origin)) {
+//      callback(null, true);
+//    } else {
+//      callback(new Error("Not allowed by CORS"));
+//    }
+//  },
+//  credentials: true
+//}));
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: allowedOrigins,
+  methods: ["GET", "POST", "PATCH", "DELETE"],
   credentials: true
 }));
 
